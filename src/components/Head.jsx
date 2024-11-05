9b3b5b5a8edd5126a5904c1720458e99af8da906
Ryan Wassom <tchewa0379@icloud.com>
2024-11-04 20:31:16 -0700

    adding og tag

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import BaseStyles from './CustomStyles/Global.styles'
+import sitethumbnail from '../images/site-thumbnails/site-thumbnail.png'
 
 export const Head = () => {
   const data = useStaticQuery(graphql`
@@ -20,6 +21,7 @@ export const Head = () => {
       <BaseStyles />
       <title>{data.site.siteMetadata.title}</title>
       <meta name="description" content={data.site.siteMetadata.description} />
+      <meta property="og:image" content={sitethumbnail} />
     </>
   )
 }
